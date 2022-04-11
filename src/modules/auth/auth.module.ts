5519bc0fedbb23b7d0dfb17b4048836d3a95dadc
CaffeineDuck <hello@samrid.me>
2022-04-11 14:29:45 +0545

    feat: uses twilio for OTP code

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,12 +4,17 @@ import { AuthController } from './auth.controller';
 import { OtpService } from './services/otp.service';
 import { JwtModule } from '@nestjs/jwt';
 import { TokenService } from './services/token.service';
+import { TwilioModule } from 'nestjs-twilio';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: 'secret',
       signOptions: { expiresIn: '1h' },
+    }),
+    TwilioModule.forRoot({
+      accountSid: '',
+      authToken: '',
     }),
   ],
   controllers: [AuthController],
--- src/modules/auth/services/otp.service.ts
+++ src/modules/auth/services/otp.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { InjectTwilio, TwilioClient } from 'nestjs-twilio';
 
 @Injectable()
 export class OtpService {
+  constructor(@InjectTwilio() private readonly twilioClient: TwilioClient) {}
+
   async generateOtp(length: number = 6): Promise<number> {
     return Math.floor(Math.random() * Math.pow(10, length));
   }
@@ -17,6 +20,11 @@ export class OtpService {
 
   async sendOtp(otp: number, phone: string): Promise<boolean> {
     console.log(otp, phone);
+    await this.twilioClient.messages.create({
+      body: `Your OTP for E-Mistri is ${otp}`,
+      to: '+977' + phone,
+      from: '+17076827899',
+    });
     return true;
   }
 }
